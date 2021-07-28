97900ecf3a18345ce242398439c889e86cde2734
Trevor Sears <trevorsears.main@gmail.com>
2021-07-28 00:35:22 -0400

    Updated missing version information in `main.ts`.

--- ts/main.ts
+++ ts/main.ts
@@ -10,8 +10,8 @@ import { HTTPRequest } from "./http-request";
  * NPM main class used for exporting this package's contents.
  *
  * @author Trevor Sears <trevorsears.main@gmail.com>
- * @version v<version>
- * @since v<version>
+ * @version v0.1.0
+ * @since v0.1.0
  */
 
 // export { ClassName } from "./class-location";
