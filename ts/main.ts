420c7ff96c1212814e5c1b2ce7f19ae8b390189a
Trevor Sears <trevors@knorrmarketing.com>
2021-09-24 16:54:41 -0400

    Added `HTTPMessage`, `HTTPRequest`, and `HTTPResponse` classes to `main.ts`.

--- ts/main.ts
+++ ts/main.ts
@@ -12,4 +12,16 @@
  * @since v0.1.0
  */
 
-// export { ClassName } from "./class-location";
+export { HTTPMessage, HTTPMessageConfig } from "./messages/message/http-message"
+export { MutableHTTPMessage } from "./messages/message/mutable-http-message"
+export { ImmutableHTTPMessage } from "./messages/message/immutable-http-message"
+
+export { HTTPRequest, HTTPRequestConfig } from "./messages/request/http-request";
+export { MutableHTTPRequest } from "./messages/request/mutable-http-request";
+export { ImmutableHTTPRequest } from "./messages/request/immutable-http-request";
+export { OutgoingHTTPRequest } from "./messages/request/outgoing-http-request";
+
+export { HTTPResponse, HTTPResponseConfig } from "./messages/response/http-response";
+export { MutableHTTPResponse } from "./messages/response/mutable-http-response";
+export { ImmutableHTTPResponse } from "./messages/response/immutable-http-response";
+export { IncomingHTTPResponse } from "./messages/response/incoming-http-response";
