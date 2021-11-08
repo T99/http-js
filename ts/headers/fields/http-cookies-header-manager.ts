eacf4dec1730922946a8652ea7e73b68595bcf24
Trevor Sears <trevors@knorrmarketing.com>
2021-11-08 16:52:33 -0500

    Added `#hasCookie` and `#getCookie` to `HTTPCookiesHeaderManager`.

--- ts/headers/fields/http-cookies-header-manager.ts
+++ ts/headers/fields/http-cookies-header-manager.ts
@@ -1,6 +1,8 @@
 import { HTTPCookie, stringifyCookie } from "../../parsing/cookie-parsing";
 import { HTTPHeadersManager } from "../http-headers-manager";
 
+function trimString(str: string): string { return str.trim(); }
+function notEmpty(str: string): boolean { return str.length !== 0; }
 
 export class HTTPCookiesHeaderManager {
 	
@@ -38,6 +40,43 @@ export class HTTPCookiesHeaderManager {
 		}
 		
 		this.headersManager.appendHeader("Set-Cookie", stringifyCookie(cookie));
+		
+	}
+	
+	public hasCookie(name: string): boolean {
+		
+		// Node pre-processes cookies into a single header field, so we only ever have to worry about the first index.
+		let cookies: string[] =
+			(this.headersManager.getHeader("Cookie") ?? [""])[0]
+				.split(";")
+				.map(trimString)
+				.filter(notEmpty);
+		
+		let cookieNames: string[] = cookies.map(
+			(cookie: string): string => cookie.split("=", 2)[0]?.trim() ?? ""
+		).filter(notEmpty);
+		
+		return cookieNames.includes(name);
+		
+	}
+	
+	public getCookie(name: string): string | undefined {
+		
+		let cookies: string[] =
+			(this.headersManager.getHeader("Cookie") ?? [""])[0]
+				.split(";")
+				.map(trimString)
+				.filter(notEmpty);
+		
+		for (let cookie of cookies) {
+			
+			let cookieComponents: [string, string] = cookie.split("=", 2).map(trimString) as [string, string];
+			
+			if (cookieComponents[0] === name) return cookieComponents[1];
+			
+		}
+		
+		return undefined;
 		
 	}
 	
