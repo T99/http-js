7148bea35e0a980b37cabd5a8ff73acff7c17cf4
Trevor Sears <trevors@knorrmarketing.com>
2022-07-12 17:01:06 -0400

    Added `eslint`.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	
+	env: {
+		
+		es6: true,
+		
+		node: true,
+		
+	},
+	
+	"extends": [
+		
+		"@t99",
+		
+	],
+	
+	parserOptions: {
+		
+		project: "ts/tsconfig.json",
+		
+	},
+	
+};
